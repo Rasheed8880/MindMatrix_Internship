8f79f976277e96ef0bbd212d1e6e6cbf2dd4de4a
Mohammed Rasheed <you@example.com>
2026-05-14 21:10:59 +0530

    Move JAL SANCHAY Android project into dedicated folder

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_mindmatrix_jalsanchaytracker_JalSanchayApp.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.mindmatrix.jalsanchaytracker.JalSanchayApp"
-)
-public final class _com_mindmatrix_jalsanchaytracker_JalSanchayApp {
-}
